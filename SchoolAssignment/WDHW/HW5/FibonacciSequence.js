738077d3e5dc6f9aebe1dd1d8d79f0173cae03d3
鄭石光 nicoakuang <125626500+nicoakuang@users.noreply.github.com>
2023-06-11 14:47:04 +0800

    FibonacciSequence.js

--- SchoolAssignment/WDHW/HW5/FibonacciSequence.js
+++ SchoolAssignment/WDHW/HW5/FibonacciSequence.js
@@ -1,8 +1,14 @@
-function fibonacci(n){
-    if(n==1||n==2)
+// Function to calculate the Fibonacci sequence of a given number n
+function fibonacci(n) {
+    // Base case: If n is 1 or 2, return 1
+    if (n === 1 || n === 2) {
         return 1;
-    else
-        return fibonacci(n-1)+fibonacci(n-2);
+    }
+    // Recursive case: Calculate the Fibonacci number by summing the previous two numbers in the sequence
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
+// Calculate the Fibonacci number for n = 40
 console.log(fibonacci(40));
