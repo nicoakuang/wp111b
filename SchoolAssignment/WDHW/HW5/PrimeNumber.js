d78a101d8d552328b11616d38ab2017f17dcefda
nicoakuang <thenickdown7@gmail.com>
2023-06-11 14:48:14 +0800

    ...

--- SchoolAssignment/WDHW/HW5/PrimeNumber.js
+++ SchoolAssignment/WDHW/HW5/PrimeNumber.js
@@ -1,9 +1,16 @@
-function isPrime(n){
-    for(var i=2;i<=Math.sqrt(n);i+=(i==2)?1:2){
-        if(n%i==0)
-            return 0;
+// Function to check if a number is prime
+function isPrime(n) {
+    // Iterate from 2 to the square root of n
+    // Check for divisibility of n by each number
+    for (var i = 2; i <= Math.sqrt(n); i += (i == 2) ? 1 : 2) {
+      // If n is divisible by i, it is not a prime number
+      if (n % i == 0) {
+        return 0; // Return 0 to indicate not prime
+      }
     }
-    return 1;
-}
-
-console.log(isPrime(9973))
+    return 1; // Return 1 to indicate prime
+  }
+  
+  // Test the isPrime function with a specific number
+  console.log(isPrime(9973));
+  
--- SchoolAssignment/WDHW/HW5/TruthTable.js
+++ SchoolAssignment/WDHW/HW5/TruthTable.js
@@ -1,14 +1,23 @@
-function truthTable(n,s=""){
-    var a=s;
-    a+="0";
-    var b=s;
-    b+="1";
-    if(n==0)
+// Function to generate a truth table with n columns
+// s is a string representing the current row in the truth table (default is an empty string)
+function truthTable(n, s = "") {
+    // Create a new string by appending "0" to the current row
+    var a = s + "0";
+
+    // Create another string by appending "1" to the current row
+    var b = s + "1";
+
+    // If n is 0, print the current row
+    if (n === 0) {
         console.log(s);
-    else{
-        truthTable(n-1,a);
-        truthTable(n-1,b);
+    } else {
+        // Recursively generate the truth table for n-1 columns with string a
+        truthTable(n - 1, a);
+
+        // Recursively generate the truth table for n-1 columns with string b
+        truthTable(n - 1, b);
     }
 }
 
+// Generate a truth table with 10 columns
 truthTable(10);
